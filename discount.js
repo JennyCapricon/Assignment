2226dfe15f2acce600edad666f0315dcc685f796
Capricon <jennynwadike894@gmail.com>
2026-03-18 05:15:10 -0700

    discount

---
+++ discount.js
@@ -0,0 +1,8 @@
+let price = 8000;
+let discountPercent = 15;
+
+let discount = price * discountPercent / 100;
+let finalPrice = price - discount;
+
+console.log("Discount:", discount);
+console.log("Final Price:", finalPrice);
