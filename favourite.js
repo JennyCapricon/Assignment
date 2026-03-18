9135f101df2ea76b036316b31f9601566d35c14a
Capricon <jennynwadike894@gmail.com>
2026-03-18 04:54:04 -0700

    init: project setup

--- favourite.js
+++
@@ -1,6 +0,0 @@
-let favoriteColor = "Black";
-let favoriteFood = "Pizza";
-let favoriteSport = "Basketball";
-let favoriteMovie = "K.Drama";
-
-console.log(`My favorite color is ${favoriteColor}, my favorite food is ${favoriteFood}, my favorite sport is ${favoriteSport}, and my favorite movie is ${favoriteMovie}.`);
