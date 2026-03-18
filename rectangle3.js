c7cf95e47a812d4da1bc780474c724b36ffd3dcb
Capricon <jennynwadike894@gmail.com>
2026-03-18 05:04:48 -0700

    rectangle3.js

---
+++ rectangle3.js
@@ -0,0 +1,8 @@
+let length = 12;
+let width = 7;
+
+let area = length * width;
+let perimeter = 2 * (length + width);
+
+console.log(`Area of the rectangle: ${area}`);
+console.log(`Perimeter of the rectangle: ${perimeter}`);
